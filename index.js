86594b62c5d15f4b55768a483d1e835cd2e3889d
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-29 01:58:57 +0600

    Working on Mongose

--- index.js
+++ index.js
@@ -429,6 +429,14 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
         app.get('/reviews/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
