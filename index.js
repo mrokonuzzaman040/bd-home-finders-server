e5ee742f52ef90cc60f842a1b694d5cde5c5e72b
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-30 11:43:51 +0600

    Remove commented out code and unused routes

--- index.js
+++ index.js
@@ -506,54 +506,6 @@ async function run() {
             const deleteResult = await offerCollection.deleteOne(query);
             res.send({ paymentResult, deleteResult });
         })
-
-        // app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
-        //     const users = await userCollection.estimatedDocumentCount();
-        //     const menuItems = await propertyCollection.estimatedDocumentCount();
-        //     const orders = await paymentCollection.estimatedDocumentCount();
-
-        //     // this is not the best way
-        //     // const payments = await paymentCollection.find().toArray();
-        //     // const revenue = payments.reduce((total, payment) => total + payment.price, 0);
-
-        //     const result = await paymentCollection.aggregate([
-        //         {
-        //             $group: {
-        //                 _id: null,
-        //                 totalRevenue: {
-        //                     $sum: '$price'
-        //                 }
-        //             }
-        //         }
-        //     ]).toArray();
-
-        //     const revenue = result.length > 0 ? result[0].totalRevenue : 0;
-
-        //     res.send({
-        //         users,
-        //         menuItems,
-        //         orders,
-        //         revenue
-        //     })
-        // })
-
-
-        // order status
-        /**
-         * ----------------------------
-         *    NON-Efficient Way
-         * ------------------------------
-         * 1. load all the payments
-         * 2. for every menuItemIds (which is an array), go find the item from menu collection
-         * 3. for every item in the menu collection that you found from a payment entry (document)
-        */
-
-        // using aggregate pipeline
-
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
     }
 }
